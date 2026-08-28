93096a43d8b406dab621e28858f9ae7332765102
Mangi-Na <crasherobjetos@gmail.com>
2026-08-28 20:22:21 -0300

    tabla enemigo

--- source/main.lua
+++ source/main.lua
@@ -6,22 +6,39 @@ x = 0
 y = 0
 img = love.graphics.newImage("assets/payaso.png")
 
+jugador = {
+    y = 0,
+    x = 0,
+    sprite = nil
+
+}
+
 
 enemigo_X = 100
 enemigo_y = 100
 enemigo_img = love.graphics.newImage("assets/rojo.png")
 
+enemigo = {
+    y = 100,
+    x = 100,
+    sprite = nil
+
+}
+
+
 function love.load()
     love.window.setMode(ANCHO_VENTANA * ESCALA, ALTO_VENTANA * ESCALA)
     love.graphics.setDefaultFilter("nearest","nearest")
     lienzo = love.graphics.newCanvas(ANCHO_VENTANA, ALTO_VENTANA)
+    jugador.sprite = love.graphics.newImage("assets/payaso.png")
+    enemigo.sprite = love.graphics.newImage("assets/rojo.png")
     
 end
 
 function love.draw()
     love.graphics.setCanvas(lienzo)
-    love.graphics.draw(img,x,y)
-    love.graphics.draw(enemigo_img, enemigo_X, enemigo_y, 0)
+    love.graphics.draw(jugador.sprite,jugador.x, jugador.y, 0)
+    love.graphics.draw(enemigo.sprite, enemigo.x, enemigo.y, 0)
     love.graphics.setCanvas()
 
     love.graphics.draw(lienzo,0,0,0,ESCALA,ESCALA)
